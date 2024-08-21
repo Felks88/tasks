22dbbfc2b7dfa5423c6a76934f0cf99a990422dc
Felks88 <sanix2t@mail.ru>
2024-08-22 00:18:41 +1000

    Add files via upload

--- module_3_3.py
+++ module_3_3.py
@@ -2,15 +2,18 @@ def print_params(a=1, b='строка', c=True):
     print(a, b, c)
 
 
+print_params()
 print_params(b=25)
 print_params(c=[1, 2, 3])
-print_params()
+
 
 values_list = ['Feliks', 1988, [9, 8]]
 values_dict = {'a': 'дата', 'b': 1988, 'c': [9, 8]}
 
+
 print_params(*values_list)
 print_params(**values_dict)
+
 
 values_list_2 = [54.32, 'Строка']
 print_params(*values_list_2, 42)
