acabd53f3ba2d5b4fd194928d30b0f714e74868f
Felks88 <sanix2t@mail.ru>
2024-10-08 22:28:38 +1100

    Add files via upload

--- module_7_4.py
+++ module_7_4.py
@@ -6,7 +6,7 @@ team1_time = 1552.512
 team2_time = 2153.31451
 tasks_total = 82
 time_avg = 45.2
-challenge_result = 'Победа команды Волшебники данных!'
+challenge_result = '????'
 
 if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
     challenge_result = 'Победа команды Мастера кода!'
