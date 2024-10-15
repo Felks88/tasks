9b22706c73c35ee2a05dc5b88b092da8b180a08a
Felks88 <sanix2t@mail.ru>
2024-10-15 22:18:01 +1100

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,12 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(i) - len(j) for i, j in zip(first, second) if len(i) != len(j))
+print(list(first_result))
+
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+print(list(second_result))
+
+
+print(list(first_result))  # Не работает второй раз
+print(list(second_result))  # Не работает второй раз
