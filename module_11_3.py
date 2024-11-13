b13f3b7aa391f8d750f06b2489ac7ff42214a400
Felks88 <sanix2t@mail.ru>
2024-11-13 23:16:04 +1000

    Add files via upload

--- module_11_3.py
+++ module_11_3.py
@@ -6,11 +6,9 @@ class Info:
         return {
             'TYPE': type(self.obj).__name__,
             'ATTRIBUTES': dir(self.obj),
-            'METHODS': [method for method in dir(self.obj) if not method.startswith('__')
-                        and callable(getattr(self.obj, method))],
+            'METHODS': [method for method in dir(self.obj) if callable(getattr(self.obj, method))],
             'MODULE': getattr(obj, '__module__')}
 
 
 obj = Info(42)
-number_info = obj.introspection_info()
-print(number_info)
+print(obj.introspection_info())
